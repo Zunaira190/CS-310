d6981e19416110ee9830d5e183490bd68b9664c9
Zunaira Imran <mynamezunaira@gmail.com>
2019-10-18 13:14:13 +0500

    My First commit

--- CS1.py
+++ CS1.py
@@ -11,3 +11,54 @@ print ("Hello Jhon"+"How are you?")
 print ("20" + "+" + "2" + "=" + "22")
 print ("I am studing BS Bioinformatics")
 print ("We are learning Object Oriented Programming from DR. Saqib Ali")
+print ('Assignment 5')
+X = 43
+X = X + 1
+print (X)
+print ('Hello World')
+print ('python learning is necessary for data analysis')
+print ('Bioinformatican must command on python for a good data analyser')
+X = 6
+print (X)
+Y = X * 7
+print (Y)
+print ('Assignment 6')
+print (4)
+print (4.3)
+print (1,000,000)
+message = 'And now for something completely different'
+n = 17
+pi = 3.141592
+print (n)
+print (pi)
+type (message)
+type (n)
+type (pi)
+print (5)
+X = 5
+print (X + 1)
+first = 10
+seconde = 15
+print (first + seconde)
+first = '100'
+seconde = '156'
+print (first + seconde)
+first = 'Test'
+seconde = 3
+print (first * seconde)
+name = input ('what is your name?\n')
+print (name)
+Hours = 35.0
+Rate = 2.75
+Pay = Hours * Rate 
+print (Pay)
+width = 17
+height = 12.0
+print (width//2)
+print (width/2)
+print (height/3)
+print (1+2*3)
+inp = input ('Enter Celsius Temperature:')
+cel = float (inp)
+fahr = (cel * 9.0) / 5.0 + 32.0
+print (fahr)
