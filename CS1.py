1fce8d6f4d7241e73b939e2bbd9107587a8c403c
Zunaira Imran <mynamezunaira@gmail.com>
2019-12-30 19:08:55 +0500

    My First commit

--- CS1.py
+++ CS1.py
@@ -170,6 +170,166 @@ whlie (True):
 average = sum(numlist) / len(numlist)
 print('Average' , average)
 
+counts = dict()
+names = ['cedf', 'asdg', 'klia']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+print(counts)
+
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names :
+    counts[name] = counts.get(name, 0) + 1
+print(counts)
+
+counts = dict()
+print('Enter a line from text:')
+line = input('')
+words = line.split()
+
+print('Words:', words)
+
+print('Countng....')
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('Counts:', counts)
+
+name = input('Enter file:')
+handle = open(name)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigword is None or count > bigcount :
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
+
+
+count = 0
+dictionary_words = dict()
+fhand = open('words.txt')
+for line in fhand:
+    words = line.split()
+    for word in words:
+        count += 1
+        if word in dictionary_words:
+            continue
+        dictionary_words[word] = counts
+
+if 'Python' in dictionary_words:
+    print('True')
+else:
+    print('False')
+
+dictionary_days = dict()                       # Initializes the dictionary
+fname = input('Enter a file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    else:
+        if words[2] not in dictionary_days:
+            dictionary_days[words[2]] = 1       # First entry
+        else:
+            dictionary_days[words[2]] += 1      # Additional counts
+
+print(dictionary_days)
+
+
+
+
+
+dictionary_addres = dict()
+fname = input('Enter file:')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('file not found:', fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    else:
+        if word[1] not in dictionary_addres:
+            dictionary_addres[words[1]] = 1
+        else:
+            dictionary_addres[words[1]] += 1
+
+print(dictionary_addres)
+
+
+
+dictionary_addresses = dict()                   # Initialize variables
+maximum = 0
+maximum_address = ''
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    quit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 2 or words[0] != 'From':
+        continue
+
+    if words[1] not in dictionary_addresses:
+        dictionary_addresses[words[1]] = 1      # First entry
+    else:
+        dictionary_addresses[words[1]] += 1     # Additional counts
+
+for address in dictionary_addresses:
+    if dictionary_addresses[address] > maximum:     # Checks if new maximum
+        # Update the maximum if needed
+        maximum = dictionary_addresses[address]
+        # Stors the address of maximum
+        maximum_address = address
+
+print(maximum_address, maximum)
+
+dictionary_domains = dict()                       # Initialize variables
+
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    quit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 2 or words[0] != 'From':
+        continue
+    else:
+        atpos = words[1].find('@')               # Position of '@'
+        domain = words[1][atpos+1:]              # Store characters after '@'
+        if domain not in dictionary_domains:
+            dictionary_domains[domain] = 1       # First entry
+        else:
+            dictionary_domains[domain] += 1      # Additional counts
+
+print(dictionary_domains)
 
 
 
